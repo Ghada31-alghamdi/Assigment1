3772aee947d49f2bde544757868685960f95ca77
Ghada31-alghamdi <s202269300@kfupm.edu.sa>
2025-09-25 19:45:33 +0300

    CSS,JS comments done

--- js/script.js
+++ js/script.js
@@ -1,2 +1,4 @@
-const date =new Date();
+const date =new Date();//creating a date object to get today date 
 alert("Welcome to Ghada Protfile The Date is: "+date.getDate()+"/"+(date.getMonth()+1)+"/"+date.getFullYear());
+//using alert to show a pop up messige contining welcome and the date using 
+//date.getDate()+"/"+(date.getMonth()+1)+"/"+date.getFullYear() methods
